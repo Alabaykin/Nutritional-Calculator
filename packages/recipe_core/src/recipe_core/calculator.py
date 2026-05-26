960d4df0d9ba6d80e07342e43ea57c2e477ea556
Alabaykin <bells.vtv@gmail.com>
2026-05-26 12:49:51 +0800

    Added raw weight summation in calculator

--- packages/recipe_core/src/recipe_core/calculator.py
+++ packages/recipe_core/src/recipe_core/calculator.py
@@ -1,4 +1,19 @@
 from .models import Recipe
+from pydantic import BaseModel
 
-def calculate_recipe(recipe: Recipe):
+class RecipeCalculationResult(BaseModel):
+    recipe_name: str
+    total_weight_raw: float
+    final_weight: float
+    total_protein: float
+    total_fat: float
+    total_carbs: float
+    total_kcal: float
+    protein_100g: float
+    fat_100g: float
+    carbs_100g: float
+    kcal_100g: float
+
+def calculate_recipe(recipe: Recipe) -> RecipeCalculationResult:
+    total_weight_raw = sum(i.weight_g for i in recipe.ingredients)
     pass
